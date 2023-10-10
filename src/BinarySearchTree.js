66a182a1d44930da4ccda08aeaac61ec415b54cf
Callie Giles <anchoredinthecrown@gmail.com>
2023-10-09 20:02:33 -0400

    add DFS code

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -62,17 +62,53 @@ class BinarySearchTree {
   }
 
   dfsInOrder(values = []) {
-    // your solution here
+    // First, process the left node recursively
+    if (this.left) {
+      values = this.left.dfsInOrder(values);
+    }
+
+    // Next, process the current node
+    values.push(this.value);
+
+    // Finally, process the right node recursively
+    if (this.right) {
+      values = this.right.dfsInOrder(values);
+    }
+
     return values;
   }
 
-  dfsPreOrder(values = []) {
-    // your solution here
+ dfsPreOrder(values=[]) {
+    // First, process the current node
+    values.push(this.value);
+
+    // Next, process the left node recursively
+    if (this.left) {
+      values = this.left.dfsPreOrder(values);
+    }
+
+    // Finally, process the right node recursively
+    if (this.right) {
+      values = this.right.dfsPreOrder(values);
+    }
+
     return values;
   }
 
   dfsPostOrder(values = []) {
-    // your solution here
+    // First, process the left node recursively
+    if (this.left) {
+      values = this.left.dfsPostOrder(values);
+    }
+
+    // Next, process the right node recursively
+    if (this.right) {
+      values = this.right.dfsPostOrder(values);
+    }
+
+    // Finally, process the current node
+    values.push(this.value);
+
     return values;
   }
 
